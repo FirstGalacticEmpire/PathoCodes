c2b4abe03c372bb36e2c404233fa2ba8726d27a9
Justyna Frączek <justynkaf553@gmail.com>
2023-12-18 19:49:03 +0100

    added search fields

--- lib/interface/idc_o_screen.dart
+++ lib/interface/idc_o_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 import 'package:patho_helper_mobile/widgets/basic_screen.dart';
 
 class ICDOScreen extends ConsumerWidget {
@@ -9,9 +7,26 @@ class ICDOScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final searchController = TextEditingController();
     return BasicScreen(
       appContext: context,
-      body: Placeholder(),
+      body: Column(children: [
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  border: const UnderlineInputBorder(),
+                  hintText: 'Search for a code here',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      //todo send request with a code
+                      //list as a response
+                    },
+                  ),
+                ))),
+      ]),
       title: 'ICD-O Screen',
       leftButton: IconButton(
         onPressed: () => Scaffold.of(context).openDrawer(),
